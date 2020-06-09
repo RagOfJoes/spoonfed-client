86270777147a79fff073cb5f9dae23d1fd5c75cd
RagOfJoes <vhsvragojos@gmail.com>
2020-06-09 03:06:56 -0700

    MOD: tweak env vars

--- next.config.js
+++ next.config.js
@@ -3,7 +3,9 @@ process.env.NODE_ENV !== 'production' && require('dotenv/config');
 module.exports = {
 	target: 'serverless',
 	env: {
-		PORT: process.env.PORT,
-		GRAPHQL_SERVER: process.env.GRAPHQL_SERVER,
+		ISSUER: process.env.ISSUER,
+		CLIENT_ID: process.env.CLIENT_ID,
+		REDIRECT_URI: process.env.REDIRECT_URL,
+		POST_LOGOUT_REDIRECT_URI: process.env.POST_LOGOUT_REDIRECT_URI,
 	},
 };
